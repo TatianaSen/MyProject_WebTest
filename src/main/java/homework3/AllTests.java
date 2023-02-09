bc798f41722ec60017cd7846442d98532ca1e0c5
Tatiana Shchetkina <senichkinatat@gmail.com>
2023-02-09 15:21:29 +0300

    Homework7

--- src/main/java/homework3/AllTests.java
+++ src/main/java/homework3/AllTests.java
@@ -2,10 +2,10 @@ package homework3;
 
 public class AllTests {
     public static void main(String[] args) {
-        Authorization authorization = Authorization.getAuthorization();
-        NewPost newPost = NewPost.getNewPost();
+        //Authorization authorization = Authorization.getAuthorization();
+        //NewPost newPost = NewPost.getNewPost();
         EditPost editPost = EditPost.getEditPost();
-        DeletePost deletePost = DeletePost.getDeletePost();
+        //DeletePost deletePost = DeletePost.getDeletePost();
 
     }
 }
